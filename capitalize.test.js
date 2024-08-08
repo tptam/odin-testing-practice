be06dcec83e026db1e0705b3798a8f0e165b40e8
tptam <99264397+tptam@users.noreply.github.com>
2024-08-09 03:10:05 +0900

    test: add edge cases

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,5 +1,17 @@
 import { capitalize } from "./capitalize.js";
 
 test("happy path", () => {
-  expect(capitalize("hello").toBe("Hello"));
+  expect(capitalize("hello")).toBe("Hello");
+});
+
+test("edge: empty string", () => {
+  expect(capitalize("")).toBe("");
+});
+
+test("edge: already capitalized", () => {
+  expect(capitalize("Hello")).toBe("Hello");
+});
+
+test("edge: non-alphabet", () => {
+  expect(capitalize("123")).toBe("123");
 });
